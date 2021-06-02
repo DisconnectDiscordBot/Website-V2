048afb881159e30d38f96e59b7a6dd533ff47bc8
Cytech <22370499+DevCytech@users.noreply.github.com>
2021-06-02 11:59:24 -0400

    Create MainLayout.js

---
+++ Components/Layouts/MainLayout.js
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+import styles from '../../styles/Layouts.module.css'
+
+export default function MainLayout({ children, page }) {
+	return (
+		<>
+			<Head>
+				{/* Meta Tags Here */}
+			</Head>
+		
+			{/* Body Elements Here */}
+		</>
+	)
+}
