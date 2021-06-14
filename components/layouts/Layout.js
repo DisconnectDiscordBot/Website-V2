79b8f924f23f2a9da1533ff100e4089a18505a8f
Cytech <22370499+DevCytech@users.noreply.github.com>
2021-06-14 13:07:26 -0400

    Update Layout.js

--- components/layouts/Layout.js
+++ components/layouts/Layout.js
@@ -6,21 +6,21 @@ export default function Layout({ children, page }) {
 		<>
 			<Head>
 				<title>Disconnect • The Discord Bot!</title>
-				<meta name="title" content="Disconnect • The Discord Bot!">
-				<meta name="description" content="Disconnect is an entertainment first Discord bot. Made to spice up any community with a new look at entertainment in Discord.">
-				<meta name='theme-color' content='#ff1c5f'>
+				<meta name="title" content="Disconnect • The Discord Bot!"/>
+				<meta name="description" content="Disconnect is an entertainment first Discord bot. Made to spice up any community with a new look at entertainment in Discord."/>
+				<meta name='theme-color' content='#ff1c5f'/>
 		
-				<meta property="og:type" content="website">
-				<meta property="og:url" content="https://disconnectbot.vercel.app/">
-				<meta property="og:title" content="Disconnect • The Discord Bot!">
-				<meta property="og:description" content="Disconnect is an entertainment first Discord bot. Made to spice up any community with a new look at entertainment in Discord.">
-				<meta property="og:image" content="https://disconnectbot.com/assets/cover.png">
+				<meta property="og:type" content="website"/>
+				<meta property="og:url" content="https://disconnectbot.vercel.app/"/>
+				<meta property="og:title" content="Disconnect • The Discord Bot!"/>
+				<meta property="og:description" content="Disconnect is an entertainment first Discord bot. Made to spice up any community with a new look at entertainment in Discord."/>
+				<meta property="og:image" content="https://disconnectbot.com/assets/cover.png"/>
 		
-				<meta property="twitter:card" content="summary_large_image">
-				<meta property="twitter:url" content="https://disconnectbot.vercel.app/">
-				<meta property="twitter:title" content="Disconnect • The Discord Bot!">
-				<meta property="twitter:description" content="Disconnect is an entertainment first Discord bot. Made to spice up any community with a new look at entertainment in Discord.">
-				<meta property="twitter:image" content="https://disconnectbot.vercel.app/assets/cover.png">
+				<meta property="twitter:card" content="summary_large_image"/>
+				<meta property="twitter:url" content="https://disconnectbot.vercel.app/"/>
+				<meta property="twitter:title" content="Disconnect • The Discord Bot!"/>
+				<meta property="twitter:description" content="Disconnect is an entertainment first Discord bot. Made to spice up any community with a new look at entertainment in Discord."/>
+				<meta property="twitter:image" content="https://disconnectbot.vercel.app/assets/cover.png"/>
 			</Head>
 
 			<div id='stars' />
