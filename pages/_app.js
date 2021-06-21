a8c600aa6cf66b6ec79c0e0bf0e73d6777ac2f92
Cytech <22370499+DevCytech@users.noreply.github.com>
2021-06-21 19:11:41 -0400

    trying to fix google

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,12 @@
 import Head from 'next/head';
 import '../styles/globals.css';
+// import { AuthProvider } from '../auth';
 
 export default function LoadApp({ Component, pageProps }) {
 	return (
 		<>
 			<Head>
-				<link rel='shortcut icon' href='/assets/favicon.ico' />
+				<link rel='shortcut icon' href='/favicon.ico' />
 				<link
 					rel='apple-touch-icon'
 					sizes='180x180'
@@ -24,7 +25,9 @@ export default function LoadApp({ Component, pageProps }) {
 					href='/assets/favicon-16x16.png'
 				/>
 			</Head>
+			{/* <AuthProvider> */}
 			<Component {...pageProps} />
+			{/* </AuthProvider> */}
 		</>
 	);
 }
