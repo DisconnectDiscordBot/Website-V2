fa7ad7cb9e124ca8fedb5bafd4656596166ebd84
Cytech <22370499+DevCytech@users.noreply.github.com>
2021-06-21 19:13:22 -0400

    favicon issues

--- pages/_app.js
+++ pages/_app.js
@@ -1,33 +1,21 @@
 import Head from 'next/head';
 import '../styles/globals.css';
-// import { AuthProvider } from '../auth';
+import { AuthProvider } from '../auth';
 
 export default function LoadApp({ Component, pageProps }) {
 	return (
 		<>
 			<Head>
-				<link rel='shortcut icon' href='/favicon.ico' />
+				<link rel='shortcut icon' href='./assets/favicon.ico' />
 				<link
 					rel='apple-touch-icon'
 					sizes='180x180'
-					href='/assets/apple-touch-icon.png'
-				/>
-				<link
-					rel='icon'
-					type='image/png'
-					sizes='32x32'
-					href='/assets/favicon-32x32.png'
-				/>
-				<link
-					rel='icon'
-					type='image/png'
-					sizes='16x16'
-					href='/assets/favicon-16x16.png'
+					href='./assets/disconnectLogo.png'
 				/>
 			</Head>
-			{/* <AuthProvider> */}
-			<Component {...pageProps} />
-			{/* </AuthProvider> */}
+			<AuthProvider>
+				<Component {...pageProps} />
+			</AuthProvider>
 		</>
 	);
 }
