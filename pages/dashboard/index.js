84381c0c36a1206f13f298fcd6a00ed304c26ce6
Cytech <22370499+DevCytech@users.noreply.github.com>
2021-06-21 11:01:27 -0400

    dashboard changes

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -1,11 +1,44 @@
 import Layout from '../../components/layouts/Layout';
 import styles from '../../styles/Redirect.module.scss';
 import Link from 'next/link';
+import Head from 'next/head';
 
 export default function Dashboard() {
 	return (
 		<>
 			<Layout page='dashboard'>
+				<Head>
+					<title>Disconnect • Dashboard</title>
+					<meta name='title' content='Disconnect • Dashboard' />
+					<meta
+						name='description'
+						content='Completely customize Disconnect using his dashboard. Coming Soon!'
+					/>
+					<meta
+						property='og:url'
+						content='https://disconnectbot.vercel.app/dashboard'
+					/>
+					<meta
+						property='og:title'
+						content='Disconnect • Dashboard'
+					/>
+					<meta
+						property='og:description'
+						content='Completely customize Disconnect using his dashboard. Coming Soon!'
+					/>
+					<meta
+						property='twitter:url'
+						content='https://disconnectbot.vercel.app/dashboard'
+					/>
+					<meta
+						property='twitter:title'
+						content='Disconnect • Dashboard'
+					/>
+					<meta
+						property='twitter:description'
+						content='Completely customize Disconnect using his dashboard. Coming Soon!'
+					/>
+				</Head>
 				<div className={styles.container}>
 					<div className={styles.inner}>
 						<h2>Dashboard</h2>
