c67c0c27ceb7698e2b3c5bee96949c038d788c7a
Cytech <22370499+DevCytech@users.noreply.github.com>
2021-06-28 22:29:36 -0400

    Added more settings

--- pages/dashboard/[id]/settings.js
+++ pages/dashboard/[id]/settings.js
@@ -25,6 +25,49 @@ export default function Dashboard() {
 						</li>
 					</ul>
 				</div>
+				<div className={styles.container}>
+					<h2>General Settings</h2>
+					<p>
+						What trivial settings can we change to mess with people?
+					</p>
+
+					<div className={styles.group}>
+						<h3>Main Settings</h3>
+						<p>How will I look today?</p>
+						<div className={styles.divider} />
+						<div className={styles.settings}>
+							<div className={styles.setting}>
+								<p className={styles.title}>Nickname</p>
+								<p>
+									Change my nickname to your server's
+									standards
+								</p>
+								<input
+									type='text'
+									id='nickname'
+									name='nickname'
+									maxLength='32'
+									placeholder='Disconnect'
+								/>
+							</div>
+							<div className={styles.setting}>
+								<p className={styles.title}>Prefix</p>
+								<p>
+									Change the prefix I respond to{' '}
+									<i>(max: 3 letters)</i>
+								</p>
+								<input
+									type='text'
+									id='prefix'
+									name='prefix'
+									maxLength='3'
+									placeholder='!'
+								/>
+							</div>
+						</div>
+						<button>Save Changes</button>
+					</div>
+				</div>
 			</DashboardLayout>
 		</>
 	);
